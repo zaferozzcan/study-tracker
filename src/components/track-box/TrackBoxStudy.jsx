d8e236e27985e92741a26d4bde36b75a81c1b967
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-08 22:39:15 -0500

    can switch between items

--- src/components/track-box/TrackBoxStudy.jsx
+++ src/components/track-box/TrackBoxStudy.jsx
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import "../../style/trackBoxUp.css";
 
 export default function TrackBoxStudy() {
+  const [xhange, setXhange] = useState("X");
+  function handleClick() {
+    xhange === "X" ? setXhange("✅") : setXhange("X");
+  }
+  console.log("X", xhange);
   return (
     <div className="track-box-up">
       <div className="track-box-up-col-title">Days</div>
       <div className="track-box-up-container">
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
         <div className="track-box-up-col">
-          <span>X</span>
+          <span onClick={handleClick}>X</span>
         </div>
       </div>
     </div>

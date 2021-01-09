a51c26486a2e25cb6273ba5ef4c0e2aeb366f2bb
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-08 21:58:03 -0500

    adjust track box maedia query

--- src/components/track-box/TrackBoxUp.jsx
+++ src/components/track-box/TrackBoxUp.jsx
@@ -5,19 +5,6 @@ import "../../style/trackBoxUp.css";
 export default function TrackBoxUp() {
   return (
     <div className="track-box-up">
-      {/* <Container className="track-box-up-container">
-        <Row className="track-box-up-row">
-          <Col className="study-todo-title">StudyToDo</Col>
-          <Col>Sunday</Col>
-          <Col>Monday</Col>
-          <Col>Tuesday</Col>
-          <Col>Wednesday</Col>
-          <Col>Thursday</Col>
-          <Col>Friday</Col>
-          <Col>Saturday</Col>
-        </Row>
-      </Container> */}
-
       <div className="track-box-up-container">
         <div className="track-box-up-col-title">StudyItemName</div>
         <div className="track-box-up-col">Sunday</div>

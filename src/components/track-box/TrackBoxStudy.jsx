390ff57e86de81c26448cdddb4bcad3ae76c62a1
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-01-08 23:23:34 -0500

    can switch between icons

--- src/components/track-box/TrackBoxStudy.jsx
+++ src/components/track-box/TrackBoxStudy.jsx
@@ -2,35 +2,87 @@ import React, { useState } from "react";
 import "../../style/trackBoxUp.css";
 
 export default function TrackBoxStudy() {
-  const [xhange, setXhange] = useState("X");
-  function handleClick() {
-    xhange === "X" ? setXhange("✅") : setXhange("X");
-  }
-  console.log("X", xhange);
+  const [xhange1, setXhange1] = useState("❌");
+  const [xhange2, setXhange2] = useState("❌");
+  const [xhange3, setXhange3] = useState("❌");
+  const [xhange4, setXhange4] = useState("❌");
+  const [xhange5, setXhange5] = useState("❌");
+  const [xhange6, setXhange6] = useState("❌");
+  const [xhange7, setXhange7] = useState("❌");
+
   return (
     <div className="track-box-up">
       <div className="track-box-up-col-title">Days</div>
       <div className="track-box-up-container">
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="1"
+            onClick={() =>
+              xhange1 === "❌" ? setXhange1("✅") : setXhange1("❌")
+            }
+          >
+            {xhange1}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="2"
+            onClick={() =>
+              xhange2 === "❌" ? setXhange2("✅") : setXhange2("❌")
+            }
+          >
+            {xhange2}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="3"
+            onClick={() =>
+              xhange3 === "❌" ? setXhange3("✅") : setXhange3("❌")
+            }
+          >
+            {xhange3}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="4"
+            onClick={() =>
+              xhange4 === "❌" ? setXhange4("✅") : setXhange4("❌")
+            }
+          >
+            {xhange4}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="5"
+            onClick={() =>
+              xhange5 === "❌" ? setXhange5("✅") : setXhange5("❌")
+            }
+          >
+            {xhange5}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="6"
+            onClick={() =>
+              xhange6 === "❌" ? setXhange6("✅") : setXhange6("❌")
+            }
+          >
+            {xhange6}
+          </span>
         </div>
         <div className="track-box-up-col">
-          <span onClick={handleClick}>X</span>
+          <span
+            id="7"
+            onClick={() =>
+              xhange7 === "❌" ? setXhange7("✅") : setXhange7("❌")
+            }
+          >
+            {xhange7}
+          </span>
         </div>
       </div>
     </div>
